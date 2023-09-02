1d82d0a82d6fc7700be2fbf892dbb941dbf00e0f
Jatin pal <jackcoder137@gmail.com>
2023-09-02 17:30:54 +0530

    box completed

--- src/MyComponents/body/Body.js
+++ src/MyComponents/body/Body.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useRef} from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from'react-bootstrap/Container';
@@ -12,14 +12,24 @@ import  card4 from'../../assets/card4.jpg'
 import  card5 from '../../assets/card5.jpg'
 import  card6 from '../../assets/card6.jpg'
 import CoverImage from '../../assets/CoverImage.png';
+
 export default function Body() {
-    
+    const sectionRef = useRef(null);
+
+  const handleScrollDown = () => {
+    if (sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <>
-    <section className='image-container-body'>
-    <img src={CoverImage} alt="Cover" />
+    <section className='image-container-body' >
+    <img src={CoverImage} alt="Cover" fixed='top'/>
+    <div className="scroll-down-button" onClick={handleScrollDown}>
+    <svg class="downward-indicator-module--downIcon--1e586 animations-module--slideUp--66da7 animations-module--slideUpStart--f5cb8" width="1.5em" height="1.5em" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"></path></svg>
+        </div>       
     </section>
-    <section>
+    <section  ref={sectionRef}>
         <Container>
         <Row className='row'>
             <Col className='col'>
